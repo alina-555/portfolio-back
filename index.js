3bc91dba302d8a01a8bfef2ddebbc63e8da9a7d8
Alina <grigoryanalina2003@gmail.com>
2025-12-26 14:42:41 +0300

    third commit

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-import express from 'express'; 
-import cors from 'cors'; 
+import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import mailRouter from './router/mail.js';
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-
-
-
-
-
 
 app.use(express.json());
-
 app.use(cors());
 
+// test route
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
 
-
+// routes
 app.post('/send-email', mailRouter);
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+// ❌ ՉԿԱ app.listen
+export default app;
